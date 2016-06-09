d0a9f29aecb9f686f25d8431d73a66c8524640b1
amma35 <amandine.manceau@infotel.com>
2016-06-09 17:36:32 +0200

    Add security

--- ajax/dropdownAllItems.php
+++ ajax/dropdownAllItems.php
@@ -32,6 +32,8 @@ include ('../../../inc/includes.php');
 header("Content-Type: text/html; charset=UTF-8");
 Html::header_nocache();
 
+Session::checkLoginUser();
+
 Plugin::load('racks', true);
 if (isset($_POST["modeltable"]) && !empty($_POST["modeltable"])) {
    $rand       = mt_rand();
